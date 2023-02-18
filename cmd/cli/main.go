1a73af20d6abe38e537a433a9244106fc9a6bc19
alijkdkar <behzad.bluekian@gmail.com>
2023-02-18 22:55:00 +0330

    redis message Broker In CLI

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,44 +2,64 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 
 	"github.com/alijkdkar/AI-File-DB-Go-Core/pkg/entity"
+	"github.com/alijkdkar/AI-File-DB-Go-Core/pkg/utility"
 )
 
 func main() {
 
-	var b []byte = make([]byte, 1)
-	for {
+	// 	if string(b) == "a" {
 
-		os.Stdin.Read(b)
-		if string(b) == "a" {
+	// 		go func() {
+	// 			file := entity.File{}
+	// 			if hashValue, file, err := file.Load("A.jpg").GetHash(); err == nil {
+	// 				fmt.Println("lent :", len(hashValue))
+	// 				file.SaveHash("", "Hashed.txt")
+	// 			}
+	// 		}()
 
-			go func() {
+	// 	} else if string(b) == "b" {
+
+	// 		go func() {
+	// 			file1 := entity.File{}
+	// 			_, f, er := file1.Load("Hashed.txt").GetTextPlain()
+	// 			if er == nil {
+	// 				//fmt.Println(txtplan)
+	// 				f.Save("", "b.jpg")
+	// 			} else {
+	// 				fmt.Println(er)
+	// 			}
+	// 		}()
+
+	fmt.Println("Core runnig...")
+	fmt.Println("Core ready to do mission ..")
+	fmt.Println("Stop Core CTRL+C ...")
+
+	redDB, err := utility.GetInstance()
+	if err != nil {
+		fmt.Printf("Error Occerd %v", err.Error())
+		panic("Error Occerd")
+	}
+	chann := redDB.Subcribe("HashChannel")
+
+	for m := range chann {
+		log.Printf("read m: %#v", string(m.Message))
+		//TODO: you muust to hash file that ricive from redis
+		go func() {
+			if fileName, err := redDB.Get(string(m.Message)); err == nil {
+				fmt.Println("File Id Not Founded")
 				file := entity.File{}
-				if hashValue, file, err := file.Load("A.jpg").GetHash(); err == nil {
+				if hashValue, file, err := file.Load(fileName).GetHash(); err == nil {
 					fmt.Println("lent :", len(hashValue))
 					file.SaveHash("", "Hashed.txt")
 				}
-			}()
+			}
 
-		} else if string(b) == "b" {
+		}()
 
-			go func() {
-				file1 := entity.File{}
-				_, f, er := file1.Load("Hashed.txt").GetTextPlain()
-				if er == nil {
-					//fmt.Println(txtplan)
-					f.Save("", "b.jpg")
-				} else {
-					fmt.Println(er)
-				}
-			}()
-
-		} else if string(b) == "q" {
-			panic("exit")
-		} else {
-			fmt.Print(">>>")
-		}
 	}
+	fmt.Println("app run ended")
+
 }
--- pkg/utility/dbRedis.go
+++ pkg/utility/dbRedis.go
@@ -1,24 +1,29 @@
 package utility
 
 import (
+	"log"
+
 	"github.com/mediocregopher/radix/v3"
 )
 
 type Reddis struct {
-	Clint radix.Pool
+	// Clint radix.Pool
+	Clint radix.Conn
 }
 
 func GetInstance() (*Reddis, error) {
 
-	pool, err := radix.NewPool("tcp", "127.0.0.1:6379", 10)
+	// pool, err := radix.NewPool("tcp", "127.0.0.1:6379", 10)
+	pool, err := radix.DefaultConnFunc("tcp", "127.0.0.1:6379")
+
 	if err != nil {
 		return nil, err
 	}
 
-	return &Reddis{Clint: *pool}, nil
+	return &Reddis{Clint: pool}, nil
 }
 
-func (r Reddis) Get(key string) (string, error) {
+func (r *Reddis) Get(key string) (string, error) {
 	var fileName string
 	err := r.Clint.Do(radix.Cmd(&fileName, "GET", key))
 	if err != nil {
@@ -27,8 +32,18 @@ func (r Reddis) Get(key string) (string, error) {
 	return fileName, nil
 }
 
-func (r Reddis) Set(key, val string) error {
+func (r *Reddis) Set(key, val string) error {
 
 	return r.Clint.Do(radix.Cmd(nil, "SET", key, val))
 
+}
+func (r *Reddis) Subcribe(chanName string) chan radix.PubSubMessage {
+
+	pstub := radix.PubSub(r.Clint)
+
+	msgCh := make(chan radix.PubSubMessage)
+	if err := pstub.Subscribe(msgCh, chanName); err != nil {
+		log.Fatal(err)
+	}
+	return msgCh
 }
